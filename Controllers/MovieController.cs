79eb3f7767c4636d1ecf17be913bbfd5a9347d89
Chirayu Singh <artist.chirayu@gmail.com>
2026-02-17 16:36:12 +0530

    Add project files.

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FilmArchive.Controllers
+{
+    public async Task<IActionResult> Index()
+    {
+        var movies = await _context.Movies.ToListAsync();
+        return View(movies);
+    }
+}
